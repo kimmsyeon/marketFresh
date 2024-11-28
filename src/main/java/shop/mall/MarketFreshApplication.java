11ebec7af53011fa258bcd5e259b7938404e6eb3
kimmsyeon <sykim557@naver.com>
2024-11-28 21:16:18 +0900

    project settings

---
+++ src/main/java/shop/mall/MarketFreshApplication.java
@@ -0,0 +1,13 @@
+package shop.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketFreshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketFreshApplication.class, args);
+	}
+
+}
